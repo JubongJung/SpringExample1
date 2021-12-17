d74d1121d01ba59c1ba415e9bebca98ee3c02e1c
정주봉 <wnqhdqhd123@gmail.com>
2021-12-17 22:46:24 +0900

    스프링 컨테이너와 싱글톤

---
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,46 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+    @Test
+    void configurationTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+
+        MemoryMemberRepository memberRepository = ac.getBean("memberRepository", MemoryMemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+        System.out.println("memberService = " + memberRepository1);
+        System.out.println("orderService = " + memberRepository2);
+        System.out.println("MemoryMemberRepository = " + memberRepository);
+//        memberService = hello.core.member.MemoryMemberRepository@333dd51e
+//        orderService = hello.core.member.MemoryMemberRepository@333dd51e
+//        MemoryMemberRepository = hello.core.member.MemoryMemberRepository@333dd51e
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$65898
+    }
+
+}
